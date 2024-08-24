a5c09be9dfe3f30b06c4fb13cc6e22f91143bdcd
navin1812ku <navin1812ku@gmail.com>
2024-08-24 19:27:51 +0530

    Web Configuration completed Cross-origin Resource Sharing

--- src/main/java/farmer/in/AgriMarket/config/WebConfig.java
+++ src/main/java/farmer/in/AgriMarket/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//Web configuration for cross-origin resource sharing
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
@@ -17,3 +18,4 @@ public class WebConfig implements WebMvcConfigurer {
                 .maxAge(3600);
     }
 }
+
