fe00e47f5620868626c24efc02144d16f048b358
navin1812ku <navin1812ku@gmail.com>
2024-10-06 06:40:29 +0530

    updated the main class v1

--- src/main/java/farmer/in/AgriMarket/controller/admin/AdminNewController.java
+++ src/main/java/farmer/in/AgriMarket/controller/admin/AdminNewController.java
@@ -6,15 +6,11 @@ import farmer.in.AgriMarket.service.admin.AdminNewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin")
 public class AdminNewController {
-
     @Autowired
     private AdminNewService adminNewService;
 
--- src/main/java/farmer/in/AgriMarket/controller/admin/IdDeleteController.java
+++ src/main/java/farmer/in/AgriMarket/controller/admin/IdDeleteController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/admin/deleteId")
 public class IdDeleteController {
-
     @Autowired
     private IdDeleteService idDeleteService;
 
